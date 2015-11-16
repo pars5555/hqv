39eff36be437f6946f94b4d1c4587c88d20b5f99
AramAnd <aramandreassian@gmail.com>
2015-11-16 12:57:24 +0400

    11

---
+++ modules/admin/templates/compile/4cce5c4dcaa580f67679166539eb4f8d28c354e0_0.file.home.tpl.php
@@ -0,0 +1,27 @@
+<?php /* Smarty version 3.1.27, created on 2015-11-15 12:25:03
+         compiled from "D:\xampp\htdocs\hqv\modules\admin\templates\main\home.tpl" */ ?>
+<?php
+/*%%SmartyHeaderCode:2026456486b900161b7_51569636%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '4cce5c4dcaa580f67679166539eb4f8d28c354e0' => 
+    array (
+      0 => 'D:\\xampp\\htdocs\\hqv\\modules\\admin\\templates\\main\\home.tpl',
+      1 => 1447586691,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2026456486b900161b7_51569636',
+  'has_nocache_code' => false,
+  'version' => '3.1.27',
+  'unifunc' => 'content_56486b900185e9_81451906',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_56486b900185e9_81451906')) {
+function content_56486b900185e9_81451906 ($_smarty_tpl) {
+
+}
+}
+?>
