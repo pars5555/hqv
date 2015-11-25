924cf4cfb015099c18e3d1120f636f8740e0d644
vahagns <vahagn.sookiasian@naghashyan.com>
2015-11-26 03:20:02 +0400

    123

--- modules/admin/htdocs/js/loads/snippets/IndexLoad.class.js
+++ modules/admin/htdocs/js/loads/snippets/IndexLoad.class.js
@@ -19,7 +19,7 @@ NGS.createLoad("admin.loads.snippets.index", {
             var am = $('#phrase_am_' + rowId).val();
             var ru = $('#phrase_ru_' + rowId).val();
             NGS.action('admin.actions.snippets.set_snippet_value', {rowId: rowId, phrase_en: en, phrase_am: am, phrase_ru: ru});
-            $.post("http://hqv.am/dyn/main/do_set_snippet_value", {rowId: rowId, phrase_en: en, phrase_am: am, phrase_ru: ru}, function (data) {
+            $.post("http://hanraqve.com/dyn/main/do_set_snippet_value", {rowId: rowId, phrase_en: en, phrase_am: am, phrase_ru: ru}, function (data) {
 
             });
 
